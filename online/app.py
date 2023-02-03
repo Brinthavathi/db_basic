dca69a5d3e9e42944732ca2115aed42d24cf93f6
Admin <admin@Admins-MacBook-Air-4.local>
2023-02-03 10:51:36 +0530

    adding files.

---
+++ online/app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,redirect
+
+import sqlite3 as sql
+
+
+app=Flask(__name__)
+
+@app.route("/")
+def fun():
+
+    conn = sql.connect('user.db')
+    conn.row_factory=sql.Row
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM post")
+    data=cur.fetchall()
+    return render_template("index.html",data=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
